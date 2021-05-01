85cbda596724aab22d17c6c5e6e76bb5a4fafead
labo-China <1784574357@qq.com>
2021-05-01 12:04:49 +0800

    Rewrite the implement of default handler

--- Interfaces.py
+++ Interfaces.py
@@ -1,15 +1,13 @@
-from Structs import Request, BytesInterface
+from Structs import Request, Interface
 
 
-class DefaultInterface(BytesInterface):
-    def __init__(self):
-        super().__init__(lambda x: None)
+def default(request: Request):
+    from datetime import datetime
+    req = repr(request)
+    time = datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')
+    return f'HTTP/1.1 200 OK\r\nDate:{time}\r\n' \
+           f'Content-Length:{len(req.encode("utf-8"))}\r\n' \
+           f'Content-Type:text/html;charset=utf8\r\n\r\n{req}'.encode('utf-8')
 
-    def execute(self, request: Request):
-        from datetime import datetime
-        req = repr(request)
-        time = datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')
-        return f'HTTP/1.1 200 OK\r\nDate:{time}\r\n' \
-               f'Content-Length:{len(req.encode("utf-8"))}\r\n' \
-               f'Content-Type:text/html;charset=utf8\r\n\r\n{req}'.encode(
-                'utf-8')
+
+DefaultInterface = Interface(default, Request)
