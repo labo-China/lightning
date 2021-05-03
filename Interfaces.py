2078bfaaef0b7d2d6dc52fc3ac3ecbe744c2f5b7
labo-China <1784574357@qq.com>
2021-05-03 14:30:20 +0800

    Rewrite default interface with Response

--- Interfaces.py
+++ Interfaces.py
@@ -1,13 +1,8 @@
-from Structs import Request, Interface
+from Structs import Request, Interface, Response
 
 
 def default(request: Request):
-    from datetime import datetime
-    req = repr(request)
-    time = datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')
-    return f'HTTP/1.1 200 OK\r\nDate:{time}\r\n' \
-           f'Content-Length:{len(req.encode("utf-8"))}\r\n' \
-           f'Content-Type:text/html;charset=utf8\r\n\r\n{req}'.encode('utf-8')
+    return Response(content = repr(request))
 
 
 DefaultInterface = Interface(default, Request)
