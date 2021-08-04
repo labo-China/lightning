61dfbfdb21fb86e2238a5c6a03faf7209b73a1b6
labo-China <1784574357@qq.com>
2021-08-04 16:33:27 +0800

    Removed unused importing.

--- demo.py
+++ demo.py
@@ -1,5 +1,4 @@
-from lightning import Node, Request, Response, Server, Folder, Interface, HookedSocket
-from typing import Callable
+from lightning import Node, Request, Response, Server, Folder
 
 # author: 程鹏博 景炎 2010
 # date 2021-4-9
