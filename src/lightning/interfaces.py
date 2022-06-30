73f1196d4233cb80560ecfd37fdbe161cef42d10
labo-China <1784574357@qq.com>
2022-06-30 23:40:35 +0800

    Replaced Folder class with the new StorageView class in order to get better performance

--- src/lightning/interfaces.py
+++ src/lightning/interfaces.py
@@ -49,115 +49,74 @@ class File(Interface):
             request.conn.sendfile(file)
         request.conn.close()
 
-    def __repr__(self) -> str:
-        return f'File[{self.path}]'
 
+class StorageView(Interface):
+    def __init__(self, root: str, depth: int = 0, enable_view: bool = True, allow_exceeded_links: bool = False, *rules):
+        """
+        :param root: the root directory
+        :param depth: the max depth (Note: path like '/foo/bar' has depth of 2)
+        :param enable_view: whether the interface will return an HTML page to list files when requesting a directory
+        :param rules: fnmatch-style file patterns
+        """
+        self.root = pathlib.Path(root)
+        self.depth = depth
+        self.enable_view = enable_view
+        self.allow_exceed_links = allow_exceeded_links
+        self.rules = rules
+        super().__init__(self.main)
 
-class Folder(Node):
-    FilterType = Union[Callable[[str], bool], Pattern, str]
-    _FilterParam = Union[Iterable[FilterType], FilterType]
+    def main(self, request: Request):
+        # return 403-Forbidden if depth is given and request is deeper than it
+        if self.depth:
+            p = pathlib.Path(request.path)
+            depth = len(p.parts)
+            if depth > self.depth:
+                return Response(403)
 
-    def __init__(self, path: str, file_depth: int = -1, lazy: bool = True, update_time: int = 60,
-                 file_filter: _FilterParam = None, file_blocker: _FilterParam = None):
-        self.path = path + '/' if not path.endswith('/') else path
-        self.dirname = basename(path.removesuffix('/'))
-        self.file_depth = file_depth
-        self.lazy = lazy
-        self.update_time = update_time
-        self.last_update = 0
-        self.filter = self._convert_filter(file_filter or '.')
-        self.blocker = self._convert_filter(file_blocker or [])
-        self._file_map = {}
+        path = self.root.joinpath(request.path)
+        if not path.exists():
+            return Response(404)
 
-        try:
-            listdir(self.path)
-        except PermissionError:
-            self.status = 403
-        except FileNotFoundError:
-            self.status = 404
-        else:
-            self.status = 0
-
-        super().__init__(interface_map = self.get_map if lazy else self.load_file(),
-                         default_interface = Interface(self.default))
-
-    @staticmethod
-    def _convert_filter(obj: _FilterParam) -> Iterable[Callable[[str], bool]]:
-        if isinstance(obj, str) or not hasattr(obj, '__iter__'):
-            obj: list[Folder.FilterType] = [obj]
-
-        def convert(f: Folder.FilterType) -> Callable[[str], bool]:
-            if isinstance(f, str):
-                # Convert string to regex pattern
-                # For string starts with "*.", converter will recognize it as a file suffix
-                string = r'.*?\.{}$'.format(f.split('*.', 1)[-1])
-                f = re.compile(string) if f.startswith('*.') else re.compile(f)
-            if isinstance(f, Pattern):
-                return lambda s: bool(f.match(s))
+        if path.is_file():
+            file = File(str(path))
+            return file.download(request)
+        elif path.is_dir():
+            if self.enable_view:
+                return Response(content = self.render(request, path), header = {'Content-Type': 'text/html'})
             else:
-                if not callable(f):
-                    raise TypeError('Filter must be a string, regex pattern or callable object')
-                return f
+                return Response(403)
+        elif path.is_symlink():
+            resolved = path.resolve()
+            if (not self.allow_exceed_links) or self.root in resolved.parents:
+                pass
+            else:
+                return Response(403)
 
-        return set(map(convert, obj))
+    def get_fd_set(self, path: pathlib.Path):
+        folder, file = set(), set()
+        fd: os.DirEntry
+        for fd in scandir(path):
+            name = basename(fd.path.removesuffix('/'))
+            if fd.is_dir():
+                folder.add(name)
+            else:
+                for r in self.rules:
+                    if not fnmatch.fnmatch(name, r):
+                        break
+                else:
+                    file.add(name)
+        return folder, file
 
-    def _is_passable(self, string: str) -> bool:
-        for filter_ in self.filter:
-            if filter_(string):
-                break
-        else:
-            return False
-        for blocker in self.blocker:
-            if blocker(string):
-                return False
-        else:
-            return True
-
-    def _filter(self, seq: Iterable[str]) -> Iterable[str]:
-        return filter(self._is_passable, seq)
-
-    def load_file(self) -> dict[str, Interface]:
-        if self.file_depth == 0:
-            return {}
-        m = {}
-        try:
-            for name in listdir(self.path):
-                abs_path = self.path + name
-                if isfile(abs_path):
-                    if self._is_passable(name):
-                        m.update({'/' + name: File(abs_path)})
-                elif isdir(abs_path) and (self.file_depth == -1 or self.file_depth >= 2):
-                    next_file_depth = self.file_depth - 1 if self.file_depth != -1 else -1
-                    m.update({'/' + name: Folder(abs_path, file_depth = next_file_depth, file_filter = self.filter,
-                                                 file_blocker = self.blocker, lazy = self.lazy)})
-        except PermissionError:
-            return {}
-        except FileNotFoundError:
-            return m
-        return m
-
-    def get_map(self) -> dict[str, Interface]:
-        if not self._file_map or time.time() - self.last_update > self.update_time:
-            self.update_map()
-        return self._file_map
-
-    def update_map(self):
-        logging.info(f'Updating the map of {self.path}...')
-        mapping = self.load_file()
-        self._file_map = mapping
-        self.last_update = time.time()
-
-    @staticmethod
-    def generate_default(request: Request, file_list: dict) -> str:
-        prev_url = request.url.removesuffix('/').rsplit('/', 1)[0]
+    def render(self, request: Request, path: pathlib.Path) -> str:
+        prev_url = pathlib.PurePosixPath(request.url).parent
         content = f'<html><head><title>Index of {request.url}</title></head><body bgcolor="white">' \
                   f'<h1>Index of {request.url}</h1><hr><pre><a href="{prev_url}">../</a>\n'
-        folder = filter(lambda f: isinstance(f, Folder), file_list.values())
-        file = filter(lambda f: isinstance(f, File), file_list.values())
-        for x in folder:
-            content += f'<a href="{request.url + x.dirname}">{x.dirname}/</a>\n'
-        for y in file:
-            content += f'<a href="{request.url + y.filename}">{y.filename}</a>\n'
+
+        folder, file = self.get_fd_set(path)
+        for x in sorted(folder):
+            content += f'<a href="{request.url + x}">{x}/</a>\n'
+        for y in sorted(file):
+            content += f'<a href="{request.url + y}">{y}</a>\n'
         return content + '</pre></body></html>'
 
     def default(self, request: Request) -> Response:
