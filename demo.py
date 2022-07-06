7e8bbe0570049e7d12fdeb00bb97b66bd3106508
labo-China <1784574357@qq.com>
2022-07-06 13:11:43 +0800

    test(demo): add some test entries and make '/storage' work on non-Windows platforms.

--- demo.py
+++ demo.py
@@ -1,5 +1,6 @@
 from src.lightning import Request, Response, Server, StorageView, Debug
 import logging
+import os
 
 logging.basicConfig(level = 'INFO', format = '[%(levelname)s](%(funcName)s) %(message)s')
 # author: 程鹏博 景炎 2010
@@ -10,22 +11,48 @@ print(f'You can visit this server at {a.addr}')
 
 @a.bind('/test')
 def hello_world(request: Request) -> Response:
-    s = f'hello world! from {request.addr}'
-    return Response(content = s)  # this response is sended by Respose class
+    s = f'Hello world! from {request.addr}⚡'
+    return Response(content = s)
 
 
-@a.bind('/raw_test', ['GET', 'POST'])
-def raw_hello(request: Request):
-    s = f'hello world! from {request.addr}'
+@a.bind('/byte_test')  # The interface sends data in the form of bytes
+def binary_test(request: Request):
+    b = bytes(f'Hello world! from {request.addr}⚡', encoding = 'utf-8')
+    return Response(content = b)
+
+
+@a.bind('/raw_test')  # The interface sends data by directly control socket object
+def raw_test(request: Request):
+    s = f'Hello world! from {request.addr}⚡'
     request.conn.sendall(Response(content = s).generate())  # this response is sended by base socket function
 
 
-@a.bind('/crash')
-def crash(request: Request):
-    return str(1 / 0) + request.path
+@a.bind('/method_test', ['POST'])  # The interface could only be accessed by using POST method
+def method_test(request: Request):
+    s = f'Hello world! from {request.addr}⚡'
+    return Response(content = s)
 
 
-a.bind('/storage', StorageView('C:/'))
+@a.bind('/strict_test', strict = True)  # The interface doesn`t allow any extra path
+def strict_test(request: Request):
+    s = f'Hello world! from {request.addr}⚡'
+    return Response(content = s)
+
+
+@a.bind('/crash_test')  # The interface will raise an error while processing
+def crash_test(request: Request):
+    s = f'Hello world! from {request.addr}⚡' + str(1 / 0)
+    return Response(content = s)
+
+
+@a.bind('/fallback_test',  # The interface will raise an error but return a normal response from fallback
+        fallback = lambda request: f'Hello world! from {request.addr}⚡ (fallback)')
+def fallback_test(request: Request):
+    s = f'Hello world! from {request.addr}⚡' + str(1 / 0)
+    return Response(content = s)
+
+
+a.bind('/storage', StorageView(os.path.expanduser('~')))
 a.bind('/debug', Debug)
 if __name__ == '__main__':
     a.run()
